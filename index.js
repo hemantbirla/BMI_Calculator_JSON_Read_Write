a3872e50b46111efaab20cda5e9b9d9ee0860ef5
hemantbirla <birlahemant.HB.b19@gmail.com>
2021-09-11 13:59:39 +0530

    calculating BMI

--- index.js
+++ index.js
@@ -10,6 +10,15 @@ fs.readFile('data.json', (err, data) => {                                       
             if (objData[objData.length - 1].TotalOverWeight == undefined) {     //to avoid repeataion of prev. data when calculating new entered data
                 objData.push({ "TotalOverWeight": 0 });
             }
+            for (var i = 0; i < objData.length - 1; i++) {
+                if (objData[i].BMIRange == undefined)                            //to avoid repeataion of prev. data when calculating new entered data
+                {
+                    var height = objData[i].HeightCm / 100;
+                    var weight = objData[i].WeightKg;
+                    var bmi = (weight / (height * height));
+                    console.log(bmi);
+                }
+            }
         }
         catch (err) {
             console.log('Error parsing JSON', err);
