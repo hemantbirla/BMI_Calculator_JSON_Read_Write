b1de771b2dd763b07c9d52fd2e43fbf9f9942394
hemantbirla <birlahemant.HB.b19@gmail.com>
2021-09-11 13:59:12 +0530

    creating a Observation for total overweighted

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ fs.readFile('data.json', (err, data) => {                                       
         try {
             let objData = JSON.parse(data);
             console.table(objData);                                            //printing data of JSON in tabular form    
+            if (objData[objData.length - 1].TotalOverWeight == undefined) {     //to avoid repeataion of prev. data when calculating new entered data
+                objData.push({ "TotalOverWeight": 0 });
+            }
         }
         catch (err) {
             console.log('Error parsing JSON', err);
