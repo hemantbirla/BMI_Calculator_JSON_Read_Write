ed24eeed2a95946b3f307988e47768728b477521
hemantbirla <birlahemant.HB.b19@gmail.com>
2021-10-16 09:08:18 +0530

    Total of BMI Category

--- index.js
+++ index.js
@@ -9,11 +9,12 @@ fs.readFile('data.json', (err, data) => {                                       
             console.table(objData);                                            //printing data of JSON in tabular form    
             if (objData[objData.length - 1].TotalUnderweight == undefined || 
                 objData[objData.length - 1].TotalNormalweight == undefined ||
-                objData[objData.length - 1].TotalOverWeight == undefined ||
+                objData[objData.length - 1].TotalOverweight == undefined ||
                 objData[objData.length - 1].TotalModeratelyobese == undefined ||
                 objData[objData.length - 1].TotalSeverelyobese == undefined ||
                 objData[objData.length - 1].TotalVeryseverelyobese == undefined) {     //to avoid repeataion of prev. data when calculating new entered data
-                objData.push({ "TotalOverWeight": 0 });
+                objData.push({ "TotalUnderweight": 0,"TotalNormalweight": 0,"TotalOverweight": 0,
+                "TotalModeratelyobese": 0, "TotalSeverelyobese": 0, "TotalVeryseverelyobese": 0});
             }
             for (var i = 0; i < objData.length - 1; i++) {
                 if (objData[i].BMIRange == undefined)                            //to avoid repeataion of prev. data when calculating new entered data
@@ -25,27 +26,32 @@ fs.readFile('data.json', (err, data) => {                                       
                     if (bmi <= 18.4) {
                         objData[i].BMICategory = "Underweight";
                         objData[i].Healthrisk = "Malnutrition risk";
+                        objData[objData.length - 1].TotalUnderweight += 1;
                     }
                     else if (18.5 <= bmi && bmi <= 24.9) {
                         objData[i].BMICategory = "Normal weight";
                         objData[i].Healthrisk = "Low risk";
+                        objData[objData.length - 1].TotalNormalweight += 1;
                     }
                     else if (25 <= bmi && bmi <= 29.9) {
                         objData[i].BMICategory = "Overweight";
                         objData[i].Healthrisk = "Enhanced risk";
-                        objData[objData.length - 1].TotalOverWeight += 1;
+                        objData[objData.length - 1].TotalOverweight += 1;
                     }
                     else if (30 <= bmi && bmi <= 34.9) {
                         objData[i].BMICategory = "Moderately obese";
                         objData[i].Healthrisk = "Medium risk";
+                        objData[objData.length - 1].TotalModeratelyobese += 1;
                     }
                     else if (35 <= bmi && bmi <= 39.9) {
                         objData[i].BMICategory = "Severely obese";
                         objData[i].Healthrisk = "High risk";
+                        objData[objData.length - 1].TotalSeverelyobese += 1;
                     }
                     else {
                         objData[i].BMICategory = "Veryseverely obese";
                         objData[i].Healthrisk = "Very high risk";
+                        objData[objData.length - 1].TotalVeryseverelyobese += 1;
                     }
                 }
             }
